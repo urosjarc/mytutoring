2fab981bd34f18e466fab3166444342b5d2abde6
urosjarc <jar.fmf@gmail.com>
2022-09-20 00:56:07 +0200

    Fix readme.

--- make.py
+++ make.py
@@ -3,15 +3,14 @@ import os
 from pathlib import Path
 
 root = Path('.')
-readme = []
+readme = ['# Podatkovne strukture']
 for dirs in sorted(root.iterdir()):
 	if dirs.is_dir() and not dirs.name.startswith('.'):
 		dataobject = ' '.join(dirs.name.split('_')[1:]).capitalize()
-		print(dataobject)
-		readme.append(f' * {dataobject}')
+		readme.append(f'## {dataobject}')
 		for markdown in sorted(dirs.iterdir()):
 			capter = " ".join(markdown.name.split('_')[1:]).replace('.md', '').capitalize()
-			readme.append(f'\t* [{capter}]({markdown})')
+			readme.append(f' * [{capter}]({markdown})')
 
 with open('README.md', 'w', errors='ignore') as file:
 	file.write("\n".join(readme))
